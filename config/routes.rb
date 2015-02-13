36d7aa6c23636ae83ddd7870b4e201ab2eb4f11d
Ellery Temple Jr <elreyatee@gmail.com>
2015-02-13 11:20:06 -0800

    added posts and category controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: :destroy
+  resources :categories, only: [:show, :new, :create] # no edit, update, destroy, or index 
 end
